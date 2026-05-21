37207708765e26a58131c78b0a23a639844fc9a0
shaikhazam990 <shaikhazam0990@gmail.com>
2026-05-22 02:07:38 +0530

    ..

--- Frontend/src/app/app.store.js
+++
@@ -1,8 +0,0 @@
-import { configureStore } from '@reduxjs/toolkit';
-import taskReducer from '../features/task/state/task.slice';
-
-export const store = configureStore({
-    reducer: {
-        tasks: taskReducer
-    }
-});
